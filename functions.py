629acfd5660059c0e3c18a43cc266a7908e3d395
JaCier8 <jappa414@gmail.com>
2026-01-17 19:55:25 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,20 @@
+def download_page(url):
+    return "Not implemented"
+
+def fetch_summary(page):
+    return "Not implemented"
+
+def fetch_table(page, number, first_row_id_header=False):
+    return "Not implemented"
+
+def count_words(page):
+    return "Not implemented"
+
+def relative_word_frequency(page, mode, count, chart=None):
+    return "Not implemented"
+
+def auto_count_words(page, depth, wait):
+    return "Not implemented"
+
+def lang_confidence_score(word_counts, language_words_with_frequency):
+    return "Not implemented"
